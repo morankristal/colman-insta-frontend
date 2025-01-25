aa8f0253636e4ec3da966ba0c11a322a079c6334
מורן קריסטל <morank44@gmail.com>
2025-01-25 16:23:45 +0200

    fixed refreah

--- colman-insta/src/components/ProtectedRoute.tsx
+++ colman-insta/src/components/ProtectedRoute.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import {isAuthenticated} from "../utils/isAuthenticated.ts";
 
-const ProtectedRoute: React.FC = () => isAuthenticated() ? <Outlet /> : <Navigate to="/login" />;
+export function ProtectedRoute() {
+    const [authenticated, setIsAuthenticated] = React.useState(false);
+    const [loading, setLoading] = React.useState(true);
 
+    const fetchIsAuthenticated = async () => {
+            setLoading(true);
+            const response = await isAuthenticated();
+            setIsAuthenticated(response);
+            setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchIsAuthenticated()
+    }, []);
+
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+
+    return authenticated ? <Outlet /> : <Navigate to="/login" />;
+}
 
 export default ProtectedRoute;
