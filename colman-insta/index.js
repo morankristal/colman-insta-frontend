02b4c24957448aacaac25e4bbed714b66499b430
מורן קריסטל <morank44@gmail.com>
2025-02-01 15:52:50 +0200

    added serve command

---
+++ colman-insta/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 80
+
+// Serve static files from the React dist folder
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Catch-all handler for serving index.html for React routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
